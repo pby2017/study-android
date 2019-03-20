c2c4da3a0dea07fef01022c5eb0f4cdcf1704305
ByungYongPark <qkrquddyd94@naver.com>
2019-03-20 16:40:03 +0900

    day02 패키지 구조 변경

--- day02/app/src/main/java/com/example/day02/example01/ViewAttrActivity.java
+++ day02/app/src/main/java/com/example/day02/example01/ViewAttrActivity.java
@@ -3,6 +3,8 @@ package com.example.day02.example01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.day02.R;
+
 public class ViewAttrActivity extends AppCompatActivity {
 
     @Override
--- day02/app/src/main/java/com/example/day02/example04/RadioButtonActivity.java
+++ day02/app/src/main/java/com/example/day02/example04/RadioButtonActivity.java
@@ -3,6 +3,8 @@ package com.example.day02.example04;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.day02.R;
+
 public class RadioButtonActivity extends AppCompatActivity {
 
     @Override
