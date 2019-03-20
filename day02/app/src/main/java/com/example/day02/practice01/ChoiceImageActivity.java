cdd806c38aa6ed958714eb40fb84c4edee46b870
ByungYongPark <qkrquddyd94@naver.com>
2019-03-20 16:36:56 +0900

    3일차 실습 소스코드 추가

---
+++ day02/app/src/main/java/com/example/day02/practice01/ChoiceImageActivity.java
@@ -0,0 +1,13 @@
+package com.example.day02.practice01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoiceImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_image);
+    }
+}
---
+++ day03/app/src/main/java/com/example/day03/practice01/LinearLayoutPracticeActivity.java
@@ -0,0 +1,14 @@
+package com.example.day03.practice01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.day03.R;
+public class LinearLayoutPracticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_practice);
+    }
+}
