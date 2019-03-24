4d8425f367343e28e4341a625d1364b7464f4c82
박병용 <qkrquddyd94@gmail.com>
2019-03-25 08:38:53 +0900

    [파일 수정] 파일 관리 컨벤션 일관성 유지

--- day01/app/src/main/java/com/example/myapplication/MainActivity.java
+++ day01/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,40 +1,38 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.example01.ButtonClickActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonStartButtonClickActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case R.id.button1:
-                        Toast.makeText(getApplicationContext(), "버튼 1 선택", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.button2:
-                        Toast.makeText(getApplicationContext(), "버튼 2 선택", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.button3:
-                        Toast.makeText(getApplicationContext(), "버튼 3 선택", Toast.LENGTH_LONG).show();
-                        break;
-                }
-            }
-        };
+        initView();
 
-        Button btn1 = (Button) findViewById(R.id.button1);
-        btn1.setOnClickListener(listener);
-        Button btn2 = (Button)findViewById(R.id.button2);
-        btn2.setOnClickListener(listener);
-        Button btn3 = (Button)findViewById(R.id.button3);
-        btn3.setOnClickListener(listener);
+        buttonStartButtonClickActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_start_button_click_activity:
+                startActivity(new Intent(getApplicationContext(), ButtonClickActivity.class));
+                break;
+        }
+    }
+
+    private void initView() {
+
+        buttonStartButtonClickActivity = (Button) findViewById(R.id.button_start_button_click_activity);
     }
 }
---
+++ day01/app/src/main/java/com/example/myapplication/example01/ButtonClickActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication.example01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class ButtonClickActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_click);
+
+        initView();
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_1:
+                Toast.makeText(getApplicationContext(), "버튼 1 선택", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_2:
+                Toast.makeText(getApplicationContext(), "버튼 2 선택", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_3:
+                Toast.makeText(getApplicationContext(), "버튼 3 선택", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    private void initView() {
+        btn1 = (Button) findViewById(R.id.button_1);
+        btn2 = (Button) findViewById(R.id.button_2);
+        btn3 = (Button) findViewById(R.id.button_3);
+    }
+}
