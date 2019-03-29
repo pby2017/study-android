8288d8076b40f872f1ef03fad1fb78e40e398263
박병용 <qkrquddyd94@gmail.com>
2019-03-29 11:52:02 +0900

    [파일 추가] day10 실습 코드

---
+++ day10/app/src/main/java/com/example/day10/example02/CallFragment.java
@@ -0,0 +1,26 @@
+package com.example.day10.example02;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.day10.R;
+
+public class CallFragment extends Fragment {
+
+    public CallFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_call, container, false);
+        return view;
+    }
+}
