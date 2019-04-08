8f0ddffe8a8c869891d6767f9cd4eecb77c50e7a
박병용 <qkrquddyd94@gmail.com>
2019-04-08 14:07:03 +0900

    [코드 수정] CustomChatting SQLite db 생성

--- CustomChatting/app/src/main/java/com/example/customchatting/MainActivity.java
+++ CustomChatting/app/src/main/java/com/example/customchatting/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.customchatting;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,15 +11,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.example.customchatting.friends.FriendListFragment;
+import com.example.customchatting.friends.helper.FriendsDatabaseHelper;
 import com.example.customchatting.friends.model.User;
+import com.example.customchatting.friends.table.FriendsTable.FriendEntry;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    // Database
+    private SQLiteDatabase sqLiteDatabase;
+
+    // RecyclerView component
     private ArrayList<User> userArrayList;
 
+    // View
     private BottomNavigationView mainBottomNavigationView;
 
     @Override
@@ -53,7 +62,35 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return true;
     }
 
+    private void initView() {
+        mainBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mainBottomNavigationView.inflateMenu(R.menu.bottom_navigation_menu);
+        mainBottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
     private void initialize() {
+
+        // TODO: naming
+        database();
+
+        // TODO: naming
+        recyclerViewComponent();
+    }
+
+    private void database() {
+        FriendsDatabaseHelper friendsDatabaseHelper = new FriendsDatabaseHelper(this, "androidDB", 1);
+
+        sqLiteDatabase = friendsDatabaseHelper.getWritableDatabase();
+
+        if (checkTable(sqLiteDatabase, FriendEntry.TABLE_NAME)) {
+            friendsDatabaseHelper.onUpgrade(sqLiteDatabase, 1, 2);
+            sqLiteDatabase.close();
+        } else {
+            // TODO: 테이블 생성
+        }
+    }
+
+    private void recyclerViewComponent() {
         userArrayList = new ArrayList<>();
         userArrayList.add(new User("a@a.a", String.valueOf(new Date().getTime())));
         userArrayList.add(new User("a@a.a", String.valueOf(new Date().getTime())));
@@ -63,10 +100,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         userArrayList.add(new User("a@a.a", String.valueOf(new Date().getTime())));
     }
 
+    private boolean checkTable(SQLiteDatabase db, String tableName) {
+        try {
+            db.rawQuery("SELECT * FROM " + tableName + " limit 1;", null);
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
 
-    private void initView() {
-        mainBottomNavigationView = findViewById(R.id.bottom_navigation);
-        mainBottomNavigationView.inflateMenu(R.menu.bottom_navigation_menu);
-        mainBottomNavigationView.setOnNavigationItemSelectedListener(this);
+        return true;
     }
 }
