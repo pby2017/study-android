fb0226ec7463225cd5b5671dd44a99c1e5f90037
박병용 <qkrquddyd94@gmail.com>
2019-04-10 16:57:31 +0900

    [파일 추가 / 코드 수정] CustomChatting 최종 프로젝트, 로그인 회원가입 UI

---
+++ CustomChatting/app/src/main/java/com/example/customchatting/constant/CustomConstant.java
@@ -0,0 +1,11 @@
+package com.example.customchatting.constant;
+
+public interface CustomConstant {
+    interface LogConstant{
+
+    }
+    interface ConfigConstant{
+        String LOGIN_SERVER_URL = "";
+        String CHATTING_SERVER_URL = "";
+    }
+}
