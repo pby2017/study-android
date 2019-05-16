9d2dc804257bfdad8643ddeeff8f21bb67e58179
박병용(Byungyong, Park) <qkrquddyd94@gmail.com>
2019-05-16 18:51:59 +0900

    레벨업_11(머티리얼디자인) 정리 추가

---
+++ levelup/레벨업_11(머티리얼디자인)/ProductFlavorApp/app/src/free/java/com/example/productflavorapp/GreateFeature.java
@@ -0,0 +1,11 @@
+package com.example.productflavorapp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class GreateFeature {
+
+    public void doIt(Context context){
+        Toast.makeText(context, "무료 버전에선 사용할 수 없습니다.", Toast.LENGTH_SHORT).show();
+    }
+}
